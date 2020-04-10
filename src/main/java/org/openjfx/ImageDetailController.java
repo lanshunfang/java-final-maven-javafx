ad08bd1319168f360596fbd526d5739dba81a356
001023566_HEYAO <57214111+HeyaoLiu@users.noreply.github.com>
2020-04-09 23:47:45 -0700

    [ImageDetail] add width and height properties and output the data on compiler.

--- src/main/java/org/openjfx/ImageDetailController.java
+++ src/main/java/org/openjfx/ImageDetailController.java
@@ -1,18 +1,25 @@
 package org.openjfx;
 
 import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.drew.metadata.exif.ExifThumbnailDirectory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import org.openjfx.core.*;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 
 //------------------------
 import com.drew.lang.GeoLocation;
 import com.drew.metadata.exif.GpsDirectory;
+
+import javax.imageio.ImageIO;
 //---------------------------
 //import javax.imageio.metadata.IIOMetadata;
 //import javafx.scene.media.Media.Metadata;
@@ -34,6 +41,8 @@ public class ImageDetailController {
     public Label latitude;//命名规范--naming convention
     @FXML
     public Label longitude;
+    @FXML
+    public Label cameraMakeAndModel;
 
     @FXML
     public void initialize() {
@@ -46,28 +55,47 @@ public class ImageDetailController {
             Metadata metadata = ImageMetadataReader.readMetadata((File) file);
             //ExifSubIFDDirectory directory=metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
             //Date date=directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-            ExifThumbnailDirectory thumbnailDirectory = metadata.getFirstDirectoryOfType(ExifThumbnailDirectory.class);
+            //ExifThumbnailDirectory thumbnailDirectory = metadata.getFirstDirectoryOfType(ExifThumbnailDirectory.class);
             //System.out.println("Camera owner name = "+thumbnailDirectory.getString(ExifThumbnailDirectory.TAG_CAMERA_OWNER_NAME));
             //System.out.println("make = "+thumbnailDirectory.getString(ExifThumbnailDirectory.TAG_MAKE));
+            //Directory subIFDDirectory1 = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+            /*Directory ifd0Directory1 = new ExifIFD0Directory();
+            Directory thumbDirectory1 = new ExifThumbnailDirectory();*/
+            //Integer height =
+//            System.out.println("Image Height: "+subIFDDirectory1.getString(ExifSubIFDDirectory.TAG_IMAGE_HEIGHT));
+//            System.out.println("Image Width: "+subIFDDirectory1.getString(ExifSubIFDDirectory.TAG_IMAGE_WIDTH));
 
+            BufferedImage bimg = ImageIO.read((File) file);
+            int width = bimg.getWidth();
+            int height = bimg.getHeight();
+            System.out.println(width+", "+height);
+
+            ExifIFD0Directory exifIFD0Directory=metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+            String make = "null";
+            String model = "null";
+            if (exifIFD0Directory!=null){
+                make=exifIFD0Directory.getString(ExifIFD0Directory.TAG_MAKE);
+                model=exifIFD0Directory.getString(ExifIFD0Directory.TAG_MODEL);
+            }
+            this.cameraMakeAndModel.setText(String.format("Camera Make and Model: %s, %s", make, model));
+            //System.out.println("Camera Make and Model: "+exifIFD0Directory.getString(ExifIFD0Directory.TAG_MAKE)+" : "+exifIFD0Directory.getString(ExifIFD0Directory.TAG_MODEL));
 
 
             GpsDirectory gpsDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
-            GeoLocation geoLocation = gpsDirectory.getGeoLocation();
-            if (geoLocation==null){
-                this.latitude.setText("Latitude: null");
-                this.longitude.setText("Longitude: null");
-            }else{
-                double latitude = geoLocation.getLatitude();
-                double longitude = geoLocation.getLongitude();
-                //if (latitude)
-                this.latitude.setText("Latitude: " + String.format("%.2f",latitude));
-                this.longitude.setText("Longitude: " + String.format("%.2f",longitude));
-                System.out.println("Latitude = " + latitude);
-                System.out.println("Longitude = " + longitude);
+
+            Double latitude= null;
+            Double longitude = null;//变量作用域
+
+            if (gpsDirectory!=null){//////数码image没进来，直接走到channel catch了
+                GeoLocation geoLocation = gpsDirectory.getGeoLocation();
+                if (geoLocation!=null){
+                    latitude = geoLocation.getLatitude();
+                    longitude = geoLocation.getLongitude();
+                }
             }
-
-
+            this.latitude.setText(latitude!=null?String.format("Latitude: %.2f",latitude):"");
+            this.longitude.setText(longitude!=null?String.format("Longitude: %.2f",longitude):"");
+            //1.没有location时没清值2.数码图片没有输出null 3.灯的照片输出null width
             //assertEquals(54.989666666666665, geoLocation.getLatitude(),0.001);
             //assertEquals(-1.9141666666666666, geoLocation.getLongitude(),0.001);
         });
