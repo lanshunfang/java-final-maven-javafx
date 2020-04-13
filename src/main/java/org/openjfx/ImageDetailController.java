e1373740cdb70ee5004b4df33833c1e86a32ec72
001023566_HEYAO <57214111+HeyaoLiu@users.noreply.github.com>
2020-04-13 16:34:17 -0700

    [ImageDetail] complete the height, width and format display.

--- src/main/java/org/openjfx/ImageDetailController.java
+++ src/main/java/org/openjfx/ImageDetailController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import org.openjfx.core.*;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -38,6 +39,10 @@ public class ImageDetailController {
     @FXML
     public HBox imageViewContainer;
     @FXML
+    public Label width;
+    @FXML
+    public Label height;
+    @FXML
     public Label latitude;//命名规范--naming convention
     @FXML
     public Label longitude;
@@ -48,27 +53,20 @@ public class ImageDetailController {
     public void initialize() {
         messaging.onMessage(MessageObject.SubjectEnum.ImageIdToShow, (file) -> {
             //Metadata metadata=new Metadata();//ImageMetadataReader.readMetadata((File)file);
+            //Image image =  ImageUtil.getImageViewByFile()getImageByFile((File) file, "", 600, 400);
             this.imageViewContainer.getChildren().clear();
             this.imageViewContainer.getChildren().add(
                     ImageUtil.getImageViewByFile((File) file, "", 600, 400)
             );
             Metadata metadata = ImageMetadataReader.readMetadata((File) file);
-            //ExifSubIFDDirectory directory=metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-            //Date date=directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-            //ExifThumbnailDirectory thumbnailDirectory = metadata.getFirstDirectoryOfType(ExifThumbnailDirectory.class);
-            //System.out.println("Camera owner name = "+thumbnailDirectory.getString(ExifThumbnailDirectory.TAG_CAMERA_OWNER_NAME));
-            //System.out.println("make = "+thumbnailDirectory.getString(ExifThumbnailDirectory.TAG_MAKE));
-            //Directory subIFDDirectory1 = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-            /*Directory ifd0Directory1 = new ExifIFD0Directory();
-            Directory thumbDirectory1 = new ExifThumbnailDirectory();*/
-            //Integer height =
-//            System.out.println("Image Height: "+subIFDDirectory1.getString(ExifSubIFDDirectory.TAG_IMAGE_HEIGHT));
-//            System.out.println("Image Width: "+subIFDDirectory1.getString(ExifSubIFDDirectory.TAG_IMAGE_WIDTH));
 
             BufferedImage bimg = ImageIO.read((File) file);
+
             int width = bimg.getWidth();
             int height = bimg.getHeight();
-            System.out.println(width+", "+height);
+            this.width.setText(/*"Width: "+*/""+width);
+            this.height.setText(/*"Height: "*/""+height);
+            //System.out.println(width+", "+height);
 
             ExifIFD0Directory exifIFD0Directory=metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
             String make = "null";
@@ -77,7 +75,7 @@ public class ImageDetailController {
                 make=exifIFD0Directory.getString(ExifIFD0Directory.TAG_MAKE);
                 model=exifIFD0Directory.getString(ExifIFD0Directory.TAG_MODEL);
             }
-            this.cameraMakeAndModel.setText(String.format("Camera Make and Model: %s, %s", make, model));
+            this.cameraMakeAndModel.setText(String.format("%s, %s", make, model));
             //System.out.println("Camera Make and Model: "+exifIFD0Directory.getString(ExifIFD0Directory.TAG_MAKE)+" : "+exifIFD0Directory.getString(ExifIFD0Directory.TAG_MODEL));
 
 
@@ -93,72 +91,26 @@ public class ImageDetailController {
                     longitude = geoLocation.getLongitude();
                 }
             }
-            this.latitude.setText(latitude!=null?String.format("Latitude: %.2f",latitude):"");
-            this.longitude.setText(longitude!=null?String.format("Longitude: %.2f",longitude):"");
+            if (latitude!=null && longitude!=null){
+                if (latitude>=0){
+                    this.latitude.setText(/*"Latitude: "+*/String.format("%.2f",latitude)+"° N");
+                }
+                else this.latitude.setText(/*"Latitude: "+*/String.format("%.2f",latitude)+"° S");
+
+                if (longitude>=0){
+                    this.longitude.setText(/*"Longitude: "+*/String.format("%.2f",longitude)+"° E");
+                }
+                else this.longitude.setText(/*"Longitude: "+*/String.format("%.2f",longitude)+"° W");
+            }
+            else {
+                this.latitude.setText("null");
+                this.longitude.setText("null");
+            }
+            /*this.latitude.setText("Latitude: "+(latitude!=null?String.format("%.2f",latitude):"null"));///
+            this.longitude.setText("Longitude: "+(longitude!=null?String.format("%.2f",longitude):"null"));///*/
             //1.没有location时没清值2.数码图片没有输出null 3.灯的照片输出null width
             //assertEquals(54.989666666666665, geoLocation.getLatitude(),0.001);
             //assertEquals(-1.9141666666666666, geoLocation.getLongitude(),0.001);
         });
     }
-
- /*   public void readAndDisplayMetadata(File file) {
-        try{
-            ImageInputStream iis=ImageIO.createImageInputStream(file);
-            Iterator<ImageReader> readers=ImageIO.getImageReaders(iis);
-            if (readers.hasNext()){
-                ImageReader reader=readers.next();
-                reader.setInput(iis,true);
-                IIOMetadata iioMetadata=reader.getImageMetadata(0);////////////
-                String[] names=iioMetadata.getMetadataFormatNames();
-                int length=names.length;
-                for (int i=0;i<length;i++){
-                    System.out.println("Format name:"+ names[i]);
-                    displayMetadata(iioMetadata.getAsTree(names[i]));//Node root
-                }
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-    void displayMetadata(Node root){
-        displayMetadata(root,0);
-    }
-    void indent(int level) {
-        for (int i = 0; i < level; i++)
-            System.out.print("    ");
-    }
-    void displayMetadata(Node node, int level) {
-        // print open tag of element
-        indent(level);
-        System.out.print("<" + node.getNodeName());
-        NamedNodeMap map = node.getAttributes();
-        if (map != null) {
-            // print attribute values
-            int length = map.getLength();
-            for (int i = 0; i < length; i++) {
-                Node attr = map.item(i);
-                System.out.print(" " + attr.getNodeName() +
-                        "=\"" + attr.getNodeValue() + "\"");
-            }
-        }
-        Node child = node.getFirstChild();
-        if (child == null) {
-            // no children, so close element and return
-            System.out.println("/>");
-            return;
-        }
-        // children, so close current tag
-        System.out.println(">");
-        while (child != null) {
-            // print children recursively
-            displayMetadata(child, level + 1);
-            child = child.getNextSibling();
-        }
-
-        // print close tag of element
-        indent(level);
-        System.out.println("</" + node.getNodeName() + ">");
-    }*/
 }
-
